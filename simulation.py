beb3b388780e7f48b6916c90c874cbf62f75997c
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-01-24 11:36:58 -0500

    Added code to create blank object

--- simulation.py
+++ simulation.py
@@ -1,1 +1,13 @@
-pass
+import time
+
+import pybullet as p
+
+physicsClient = p.connect(p.GUI)
+
+for x in range(1000):
+    p.stepSimulation()
+    print(x)
+    time.sleep(1/60)
+
+p.disconnect()
+
