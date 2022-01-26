39a9872afa0b6039e5fc9d60a121c7f2c7d0e966
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-01-26 17:38:02 -0500

    Integrating pyrosim

---
+++ generate.py
@@ -0,0 +1,7 @@
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("box.sdf")
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+pyrosim.End()
--- simulation.py
+++ simulation.py
@@ -4,6 +4,7 @@ import pybullet as p
 
 physicsClient = p.connect(p.GUI)
 
+p.loadSDF("box.sdf")
 for x in range(1000):
     p.stepSimulation()
     print(x)
