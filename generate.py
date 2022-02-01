e5393a6870c8d9e14c28d8e3c16a9b2dc32bb85a
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-01-31 22:10:37 -0500

    Made other robot to test joints

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ def Create_World():
     pyrosim.End()
 
 def Create_Robot():
-    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Start_URDF("body1.urdf")
     pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
     pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
     pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5], size=[length, width, height])
@@ -26,8 +26,16 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Link5", pos=[0, 0, -.5], size=[length, width, height])
     pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[0, 0, -1])
     pyrosim.Send_Cube(name="Link6", pos=[0, 0, -.5], size=[length, width, height])
+
+
+def Create_Robot1():
+    pyrosim.Start_URDF("body1.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, .5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="Link1", pos=[0.5, 0, 0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[1, 0, 0])
+    pyrosim.Send_Cube(name="Link2", pos=[0.5, 0, -0.5], size=[length, width, height])
     pyrosim.End()
 
-
-Create_Robot()
+Create_Robot1()
 Create_World()
