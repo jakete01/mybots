b9bd5a0fcbedff1c66835026352ea5ae74a2c5ef
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-02-07 14:44:16 -0500

    Fixed naming and joint tree structure from last week

--- generate.py
+++ generate.py
@@ -26,16 +26,17 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Link5", pos=[0, 0, -.5], size=[length, width, height])
     pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[0, 0, -1])
     pyrosim.Send_Cube(name="Link6", pos=[0, 0, -.5], size=[length, width, height])
-
+    pyrosim.End()
 
 def Create_Robot1():
     pyrosim.Start_URDF("body1.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[0, 0, .5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0.5, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[0.5, 0, 0.5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[1, 0, 0])
-    pyrosim.Send_Cube(name="Link2", pos=[0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
     pyrosim.End()
+
 
 Create_Robot1()
 Create_World()
