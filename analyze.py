3ddb0c5a8f91fab234156114469cdf57b8223cd8
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-02-14 12:16:56 -0500

    Working on changing motor values in the vector

--- analyze.py
+++ analyze.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot
 
 backLegSensorValues = numpy.load('./data/backLegSensorValues.npy')
 frontLegSensorValues = numpy.load('./data/frontLegSensorValues.npy')
+targetAngles = numpy.load('./data/targetAngles.npy')
 
-matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg Values', linewidth=4)
-matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg Values')
+# matplotlib.pyplot.plot(backLegSensorValues, label='Back Leg Values', linewidth=4)
+# matplotlib.pyplot.plot(frontLegSensorValues, label='Front Leg Values')
 
+matplotlib.pyplot.plot(targetAngles, label='Target Angles', linewidth=2)
 matplotlib.pyplot.legend()
-# matplotlib.pyplot.show()
+matplotlib.pyplot.show()
--- simulation.py
+++ simulation.py
@@ -15,8 +15,16 @@ robotId = p.loadURDF("body1.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
+targetAngles = np.linspace(-np.pi/4, +np.pi/4, 1000)
+targetAngleSins = np.zeros(1000)
+for i in range(1000):
+    targetAngleSins[i] = np.sin(targetAngles[i])
+
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
+
+np.save(os.path.join('./data', 'targetAngles.npy'), targetAngleSins)
+exit()
 for i in range(1000):
     p.stepSimulation()
 
@@ -24,14 +32,16 @@ for i in range(1000):
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
 
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_BackLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=-np.pi/4, maxForce=500)
+                                targetPosition=-random.uniform(-np.pi/15.0, np.pi/15.0), maxForce=500)
 
     pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_FrontLeg", controlMode=p.POSITION_CONTROL,
-                                targetPosition=np.pi/4, maxForce=500)
+                                targetPosition=random.uniform(-np.pi/15.0, np.pi/15.0), maxForce=500)
+
 
     time.sleep(1/200)
 
 np.save(os.path.join('./data', 'backLegSensorValues.npy'), backLegSensorValues)
 np.save(os.path.join('./data', 'frontLegSensorValues.npy'), frontLegSensorValues)
+
 
 p.disconnect()
