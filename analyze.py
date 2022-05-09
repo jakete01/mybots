9413f932272c491baf4556d218af7400970b4cf9
Jake Ten Eyck <56373039+jakete01@users.noreply.github.com>
2022-05-09 15:12:00 -0400

    Data analysis complete

--- analyze.py
+++ analyze.py
@@ -1,25 +1,36 @@
 import numpy
 import matplotlib.pyplot
 
-f = open('data/bestFitnessa.npy', 'r')
-runA = numpy.zeros(200)
-lines = f.readlines()
-for i in range(0, len(lines)):
-    runA[i] = float(lines[i])
-numpy.save('tempA.npy', runA)
+averageAValues = numpy.zeros(500)
+averageBValues = numpy.zeros(500)
 
-f = open('data/bestFitnessb.npy', 'r')
-runB = numpy.zeros(200)
-lines = f.readlines()
-for i in range(0, len(lines)):
-    runB[i] = float(lines[i])
-numpy.save('tempB.npy', runB)
+# Final Project Analysis
+for i in range(2, 16):
+    f1 = open('data/bestFitnessRun' + str(i) + 'a.npy', 'r')
+    f2 = open('data/bestFitnessRun' + str(i) + 'b.npy', 'r')
+    f1Lines = f1.readlines()
+    f2Lines = f2.readlines()
+    for j in range(0, 500):
+        averageAValues[j] += float(f1Lines[j])
+        averageBValues[j] += float(f2Lines[j])
 
-fitnessA = numpy.load('tempA.npy')
-fitnessB = numpy.load('tempB.npy')
+for i in range(0, 500):
+    averageAValues[i] = averageAValues[i] / 14
+    averageBValues[i] = averageBValues[i] / 14
+
+numpy.save('tempAveA.npy', averageAValues)
+numpy.save('tempAveB.npy', averageBValues)
+
+aveFitnessA = numpy.load('tempAveA.npy')
+aveFitnessB = numpy.load('tempAveB.npy')
 
 
-matplotlib.pyplot.plot(fitnessA, label='Fitness: Recurrent Connections', linewidth=2)
-matplotlib.pyplot.plot(fitnessB, label='Fitness: No Recurrent Connections', linewidth=2)
+# Plot and show data
+matplotlib.pyplot.title('Average fitness over 500 generations')
+matplotlib.pyplot.plot(aveFitnessA, label='Recurrent Connections', linewidth=2)
+matplotlib.pyplot.plot(aveFitnessB, label='No Recurrent Connections', linewidth=2)
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
+
+print(averageBValues[0])
+print(averageBValues[499])
